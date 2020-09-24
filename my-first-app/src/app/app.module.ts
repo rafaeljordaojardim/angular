02a39fdab0e23d89e75ca09ba801c18ce6c394f9
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-09-23 21:09:25 -0300

    feat: initial lectures

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessfulComponent } from './success/successful.component';
+import { ErrorComponent } from './error/error.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessfulComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-first-app/src/app/success/successful.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successful',
+  templateUrl: './successful.component.html',
+  styleUrls: ['./successful.component.css']
+})
+export class SuccessfulComponent {
+
+}
